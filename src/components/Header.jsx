e008561f5d8e628d0e6737bb5c9391aca2b8d244
Saruhan <saruhan.oymaci@hotmail.com>
2022-01-07 09:13:21 +0300

    header-revision

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,29 +32,43 @@ function Header() {
                     </div>
                 </div>
                 <div className="small">
-                    <div className="row">
-                        <div className=" col-6 ">
-                            <div className="logo">
-                                <img src={Logo} alt="" srcset="" />
-                            </div>
-                        </div>
-                        <div class="dropdown offset-5 col-1">
-                            <div className=" dropdown-toggle d-flex justify-content-end" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i className="fas fa-bars"></i>
-                            </div>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li>Home</li>
-                                <li>Collections</li>
-                                <li>How We Collect</li>
-                                <li> Sell An Antique</li>
-                                <li>Blog</li>
-                            </ul>
-                        </div>
 
-                        <div className="button col-12 ">
-                            <button className='btn'>Our Collections</button>
+
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="container-fluid">
+                            <a class="navbar-brand logo" href="#"><img src={Logo} alt="" srcset="" /></a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Collections</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">How We Collect</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#" >Sell An Antique</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#" >Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+
+                                        <div className="button">
+                                            <button className='btn'>Our Collections</button>
+                                        </div>
+
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
+                    </nav>
+
 
                 </div>
             </div>
